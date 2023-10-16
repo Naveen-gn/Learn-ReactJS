f057d9809ef4010bf7d666e70228bd92510d5c35
Naveen-gn <naveen2003gn@gmail.com>
2023-10-16 14:10:54 +0530

    Create component on new folder

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import Car from './Components/Car';
 
 function App() {
   return (
+    <>
     <h1>First App</h1>
+    <Car />
+    </>
+    
   );
 }
 
---
+++ my-first-app/src/Components/Car.js
@@ -0,0 +1,6 @@
+function Car(){
+    return(
+      <h1>This is car component</h1>
+    );
+  }
+  export default Car;
