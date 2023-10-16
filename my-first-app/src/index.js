d991d306ee9766942c0214a78f8ceb5b4e356415
Naveen-gn <naveen2003gn@gmail.com>
2023-10-16 14:08:06 +0530

    create component on already existing file

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+function Car(){
+  return(
+    <h1>This is car component</h1>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Car />
   </React.StrictMode>
 );
 
