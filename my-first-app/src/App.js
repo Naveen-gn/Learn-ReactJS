e8acc06bca96001c0eba5f061044d3cc700118e1
Naveen-gn <naveen2003gn@gmail.com>
2023-10-16 14:59:33 +0530

    Props

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -3,11 +3,12 @@ import Car from './Components/Car';
 
 
 function App() {
-  const name="Naveen";
+  const name1="Propertie1";
+  const name2="Propertie2";
   return (
     <>
     <h1>First App</h1>
-    <Car Name={name} />
+    <Car Name1={name1} Name2={name2} />
    
     </>
     
--- my-first-app/src/Components/Car.js
+++ my-first-app/src/Components/Car.js
@@ -1,8 +1,10 @@
 function Car(props){
-    const {Name}=props
+    const {Name1}=props
+    const {Name2}=props
     return(
         <>
-                <h2>{Name}</h2>
+                <h2>{Name1}</h2>
+                <h2>{Name2}</h2>
       <h1>This is car component</h1>
         </>
 
