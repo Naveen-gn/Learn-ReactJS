5414b5f3e6a5d419ccc0c1b16cf38193393118cf
Naveen-gn <naveen2003gn@gmail.com>
2023-10-30 21:46:05 +0530

    Learn inline styles

---
+++ myapp/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Container, Row, Col } from "react-bootstrap";
+function App() {
+const h1styles={
+  color:'green',
+  fontSize:'80px'
+
+}
+  return (
+   <div>
+    <h1 style={h1styles} >hii</h1>
+    </div>
+
+
+
+  );
+ 
+}
+
+export default App;
