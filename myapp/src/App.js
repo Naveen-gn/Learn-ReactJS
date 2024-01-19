eafbfca4ac6e6f0a2d77647a9f3191d1c316ba04
Naveen-gn <naveen2003gn@gmail.com>
2024-01-19 16:24:50 +0530

    React Router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,14 +2,28 @@ import './App.css';
 import Home from './Components/HomePage';
 import About from './Components/AboutPage';
 import Contact from './Components/ContactPage';
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
 function App() {
   
   return (
-    <>
-  <Home/>
-  <About/>
-  <Contact/>
-    </>
+<>
+    <Router>
+    <ul>
+        <li> <Link to='/' >Home</Link> </li>
+        <li> <Link to='/about' >About</Link> </li>
+        <li> <Link to='/contact' >Contact</Link> </li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
+
+</>    
+    
+
+    
   );
 }
 export default App;
