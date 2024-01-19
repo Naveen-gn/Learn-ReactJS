dd40a88d8a3934924b03796434390082d7b9f099
Naveen-gn <naveen2003gn@gmail.com>
2024-01-19 16:09:29 +0530

    Learn React Routers

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,31 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import Home from './Components/HomePage';
+import About from './Components/AboutPage';
+import Contact from './Components/ContactPage';
 function App() {
-  const [obj,setobj]=useState({
-    name:'apple',
-    year:'2000',
-    price:'100000',
-    Model:'2023'
-  })
-
- function changeValue(){
-  setobj(
-    (PreValues)=>{
-      return {...PreValues, name:'Samsung'}
-    }
-  )
- }
-
+  
   return (
-    <div>
-      <h1>Name: {obj.name}</h1>
-      <h1>Year: {obj.year}</h1>
-      <h1>Price: {obj.price}</h1>
-      <h1>Model: {obj.Model}</h1>
-      <button onClick={()=>{changeValue()}} >Change Name</button>
-      
-    </div>
+    <>
+  <Home/>
+  <About/>
+  <Contact/>
+    </>
   );
 }
-
 export default App;
